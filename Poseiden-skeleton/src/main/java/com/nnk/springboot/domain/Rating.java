d1d7d52caa2ef75b5b2d26ca01a4252442db4152
Aimé Malcorps <aime.malcorps@neuf.fr>
2022-12-08 18:26:43 +0100

    First Commit

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -9,4 +9,56 @@ import java.sql.Timestamp;
 @Table(name = "rating")
 public class Rating {
     // TODO: Map columns in data table RATING with corresponding java fields
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private Integer id;
+	private String moodysRating;
+	private String sandPRating;
+	private String fitchRating;
+	private Integer orderNumber;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getMoodysRating() {
+		return moodysRating;
+	}
+	public void setMoodysRating(String moodysRating) {
+		this.moodysRating = moodysRating;
+	}
+	public String getSandPRating() {
+		return sandPRating;
+	}
+	public void setSandPRating(String sandPRating) {
+		this.sandPRating = sandPRating;
+	}
+	public String getFitchRating() {
+		return fitchRating;
+	}
+	public void setFitchRating(String fitchRating) {
+		this.fitchRating = fitchRating;
+	}
+	public Integer getOrderNumber() {
+		return orderNumber;
+	}
+	public void setOrderNumber(Integer orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+	
+	public Rating() {
+		
+	}
+	
+	public Rating(String string, String string2, String string3, int i) {
+		Rating rating = new Rating();
+		rating.setOrderNumber(i);
+		rating.setMoodysRating(string);
+		rating.setSandPRating(string2);
+		rating.setFitchRating(string3);
+	}
+	
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -8,4 +8,71 @@ import java.sql.Timestamp;
 @Table(name = "rulename")
 public class RuleName {
     // TODO: Map columns in data table RULENAME with corresponding java fields
+	@Id
+	@NotBlank
+	private Integer id;
+	private String name;
+	private String description;
+	private String json;
+	private String template;
+	private String sqlStr;
+	private String sqlPart;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getJson() {
+		return json;
+	}
+	public void setJson(String json) {
+		this.json = json;
+	}
+	public String getTemplate() {
+		return template;
+	}
+	public void setTemplate(String template) {
+		this.template = template;
+	}
+	public String getSqlStr() {
+		return sqlStr;
+	}
+	public void setSqlStr(String sqlStr) {
+		this.sqlStr = sqlStr;
+	}
+	public String getSqlPart() {
+		return sqlPart;
+	}
+	public void setSqlPart(String sqlPart) {
+		this.sqlPart = sqlPart;
+	}
+	
+	public RuleName() {
+		
+	}
+	
+	public RuleName(String string, String string2, String string3, String string4, String string5, String string6) {
+		RuleName rule = new RuleName();
+		rule.setName(string);
+		rule.setDescription(string2);
+		rule.setJson(string3);
+		rule.setTemplate(string4);
+		rule.setSqlStr(string5);
+		rule.setSqlPart(string6);
+	}
+	
 }
